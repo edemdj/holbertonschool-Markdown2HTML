cb58a64e6bca8ecacf533b7ff7f67a28f7df8228
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-12-10 10:05:04 +0100

    Markdown syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-import os
-import sys
-
-def main():
-    # Check if the number of arguments is less than 2
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the input markdown file and output HTML file
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Check if the markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # If everything is fine, exit with 0 (no output)
-    sys.exit(0)
+""" markdown to html """
 
 if __name__ == "__main__":
-    main()
+
+    import sys
+    import os.path
+
+    if len(sys.argv) < 3:
+        sys.exit("Usage: ./markdown2html.py README.md README.html")
+    elif (not os.path.isfile(sys.argv[1]) or not os.path.exists(sys.argv[1])):
+        sys.exit("Missing {}".format(sys.argv[1]))
+    else:
+        text = ""
+        with open(sys.argv[1], encoding='utf-8') as md_file:
+            for line in md_file:
+                if line.split(' ')[0][0] == '#':
+                    length = len(line.split(' ')[0])
+                    heading = " ".join(line.split(' ')[1:-1]) +\
+                        line.split(' ')[-1][:-1]
+                    text += "<h{}>{}</h{}>\n".format(length, heading, length)
+        with open(sys.argv[2], 'w', encoding='utf-8') as html_file:
+            html_file.write(text)
