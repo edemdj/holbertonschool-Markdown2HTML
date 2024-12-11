0f8b8d5845267e4a345f68bf7cb4387c7a8c345f
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-12-11 15:32:16 +0100

    Ordered listing 2

--- markdown2html.py
+++ markdown2html.py
@@ -1,40 +1,64 @@
 #!/usr/bin/python3
-""" markdown to html """
+"""Markdown to HTML converter"""
 
 if __name__ == "__main__":
 
     import sys
-    import os.path
+    import os
 
     if len(sys.argv) < 3:
         sys.exit("Usage: ./markdown2html.py README.md README.html")
-    elif (not os.path.isfile(sys.argv[1]) or not os.path.exists(sys.argv[1])):
-        sys.exit("Missing {}".format(sys.argv[1]))
-    else:
-        text = []
-        with open(sys.argv[1], encoding='utf-8') as md_file:
-            i = 0
-            for line in md_file:
-                if line.split(' ')[0][0] == '#':
-                    length = len(line.split(' ')[0])
-                    if length < 7:
-                        heading = " ".join(line.split(' ')[1:-1]) +\
-                            line.split(' ')[-1][:-1]
-                        text.append("<h{}>{}</h{}>".format(
-                            length, heading, length))
-                    else:
-                        text.append(line)
-                elif line.split(' ')[0][0] == '-':
-                    lista = line.split(' ')[1][:-1]
-                    if i == 0:
-                        text.append("<ul>\n<li>{}</li>".format(lista))
-                        text.append("</ul>")
-                    else:
-                        text.insert(-1, "<li>{}</li>".format(lista))
-                    i += 1
-                else:
-                    text.append(line)
+    elif not os.path.isfile(sys.argv[1]):
+        sys.exit(f"Missing {sys.argv[1]}")
 
-        with open(sys.argv[2], 'w', encoding='utf-8') as html_file:
-            html_file.write('\n'.join(text)+'\n')
+    text = []
+    in_list = False  
+    list_type = None  
+
+    with open(sys.argv[1], encoding='utf-8') as md_file:
+        for line in md_file:
+            line = line.rstrip()
+
             
+            if line.startswith("#"):
+                heading_level = len(line.split(' ')[0])
+                if heading_level < 7:
+                    heading_text = " ".join(line.split(' ')[1:])
+                    text.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+
+            
+            elif line.lstrip().startswith(tuple(f"{i}." for i in range(1, 10))):
+                if not in_list or list_type != "ol":
+                    if in_list:
+                        text.append(f"</{list_type}>")
+                    in_list = True
+                    list_type = "ol"
+                    text.append("<ol>")
+                list_item = line.lstrip().split('. ', 1)[1]
+                text.append(f"<li>{list_item}</li>")
+
+            
+            elif line.lstrip().startswith("- "):
+                if not in_list or list_type != "ul":
+                    if in_list:
+                        text.append(f"</{list_type}>")
+                    in_list = True
+                    list_type = "ul"
+                    text.append("<ul>")
+                list_item = line.lstrip()[2:]
+                text.append(f"<li>{list_item}</li>")
+
+            
+            else:
+                if in_list:
+                    text.append(f"</{list_type}>")
+                    in_list = False
+                    list_type = None
+                text.append(line)
+
+        
+        if in_list:
+            text.append(f"</{list_type}>")
+
+    with open(sys.argv[2], 'w', encoding='utf-8') as html_file:
+        html_file.write('\n'.join(text) + '\n')
